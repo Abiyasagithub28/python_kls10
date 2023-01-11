f188a9905820cecce0c812f81f24c1c46e5f92dc
Abiyasagithub28 <radensalaf@gmail.com>
2023-01-12 00:58:08 +0700

    yt

---
+++ materi youtube/input user.py
@@ -0,0 +1,15 @@
+#menginput data dari user
+
+#menginput tipe data str
+data= input("masukan nama :")
+print("nama =",data,",type =",type(data))
+
+#jika ingin mengambil int,maka
+
+angka=int(input("masukan angka :"))
+print("angka",angka,"type=",type(angka))
+
+#bagaimana jika boolean?
+
+biner=bool(int(input("masukan nilai boolean :")))
+print("biner=",biner,"type=",type(biner))
